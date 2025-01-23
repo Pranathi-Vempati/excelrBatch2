f39cb32b227b27b6581c3badf3e1b238cead2416
Pranathi-Vempati <pranathivempati1104@gmail.com>
2025-01-23 09:38:31 +0530

    Add files via upload
    reversing integer

---
+++ ltcode002.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10; // Get the last digit
+            reversedNumber = reversedNumber * 10 + digit; // Append digit to reversed number
+            number /= 10; // Remove the last digit
+        }
+
+        System.out.println("Reversed number: " + reversedNumber);
+        scanner.close();
+    }
+}
